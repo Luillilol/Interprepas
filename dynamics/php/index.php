8b2972d3eb8eabfa45f03b364c6592664f55d3f7
Luillilol <luilli359@gmail.com>
2022-02-10 21:31:11 -0600

    Creación de carpetas

--- dynamics/php/index.php
+++ dynamics/php/index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Maratón</title>
 </head>
 <body>
+    <h1>INTERPREPAS OWO </h1>
     <?php
-        include("dynamics/config.php");
+        include("dynamics/php/config.php");
         $con=connect();
         $sql="Create TABLE a(a int)";
         $result=mysqli_query($con,$sql);
