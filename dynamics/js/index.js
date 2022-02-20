2c842870e8b359ba466f5cab7d41802059114262
Luillilol <luilli359@gmail.com>
2022-02-20 02:39:44 -0600

    FEAT:Avance index: pantalla caracteristicas del juego

--- dynamics/js/index.js
+++ dynamics/js/index.js
@@ -2,12 +2,27 @@ window.addEventListener("load", ()=>{
     let botonJugar = document.getElementById("idBotonJugar");
     let botonInstrucciones = document.getElementById("idBotonInstrucciones");
     let botonCredito = document.getElementById("idBotonCreditos");
+    let fondoDegradado = document.getElementById("id_capaOscura");
+    let divCaractJuego = document.getElementById("id_caractJuego");
+    let botonForm = document.getElementById("boton");
+    let cerrarVentana = document.getElementById("cerrarVentana");
 
-    console.log("hola");
+    //Eventos de los botónes del menú (Jugar, Instrucciones y )
     botonJugar.addEventListener("click", ()=>{
         console.log("Hola pagina a jugar");
+        fondoDegradado.style.visibility="visible";
+        divCaractJuego.style.visibility="visible";
+    });
+
+    botonForm.addEventListener("click", ()=>{
         window.location.assign("./templates/juego.html");
     });
+    cerrarVentana.addEventListener("click", ()=>{
+        fondoDegradado.style.visibility="hidden";
+        divCaractJuego.style.visibility="hidden";
+    });
+    
+
 
     botonInstrucciones.addEventListener("click", ()=>{
         console.log("Hola pagina a instrucciones");
