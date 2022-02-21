2cc563f19e63d565c1224395c0beb4fba9588fbd
andreco <andreco@localhost.localdomain>
2022-02-21 06:37:31 -0600

    Cambio de style.css

--- dynamics/js/juego.js
+++ dynamics/js/juego.js
@@ -81,7 +81,8 @@ window.onload = function() {
         if (pantalla == "mainMenu") {
             mainMenu();
         } else if (pantalla == "tablero") {
-            tabelro();
+            //tabelro();
+            ;
         } else if (pantalla == "gameOver") {
             gameOver();
         }
